17bdc54b93f3ab3da6bf2c3972431b103eedc8e0
Sebastian Korsak <49608786+BlackPianoCat@users.noreply.github.com>
2024-07-20 09:45:18 +0200

    Add files via upload

---
+++ nucleosome_interpolation.py
@@ -0,0 +1,161 @@
+#########################################################
+######## SEBASTIAN KORSAK, KRZYSZTOF BANECKI ############
+#################### WARSAW 2024  #######################
+#########################################################
+
+import numpy as np
+from utils import *
+from initial_structure_tools import *
+from plots import *
+from tqdm import tqdm
+
+
+def makeUnit(x):
+    """Normalize vector to norm 1"""
+    return x / np.linalg.norm(x)
+
+
+def get_perpendicular(vec):
+    """Get random perpendicular vector to vec"""
+    if vec[0] != 0 or vec[1] != 0:
+        return np.array([vec[1], -vec[0], 0])
+    else:
+        return np.array([vec[2], 0, -vec[0]])
+
+
+def get_perp_component(x, v):
+    """Component of x orthogonal to v. Result is perpendicular to v"""
+    return x - np.dot(x, v) / np.dot(v, v) * v
+
+
+class NucleosomeInterpolation:
+    def __init__(self, V, bw, max_nucs_per_bead=4, nuc_radius=0.1, points_per_nuc=20, phi_norm=np.pi/5):
+        self.V, self.bw = V, bw
+        self.max_nucs_per_bead, self.nuc_points = max_nucs_per_bead, points_per_nuc
+        self.nuc_r, self.phi_norm = nuc_radius, phi_norm
+    
+    def make_helix(self, r, theta, z0):
+        x = r * (-np.cos(theta)+1)
+        y = r * np.sin(theta)
+        z = z0 * theta / theta[-1]
+        return np.vstack([x, y, z]).T
+    
+    def min_max_scale(self,array):
+        # Find the minimum and maximum values of the array
+        min_val = np.min(array)
+        max_val = np.max(array)
+        
+        # Scale the array to the interval [-1, 1]
+        scaled_array = (array - min_val) / (max_val - min_val)
+        return scaled_array
+
+    def move_structure_to(self, struct, p0, p1, p2):
+        """
+        The structure will be placed assuming new X-axis would be defined by vector p2-p1.
+        New Y-axis would be defined by part of p0-p1 vector orthogonal to p2-p1.
+        """
+        if p1[0]==p2[0] and p1[1]==p2[1] and p1[2]==p2[2]:
+            raise(Exception("Starting point and the ending point must be different!"))
+        if p1[0]==p0[0] and p1[1]==p0[1] and p1[2]==p0[2]:
+            raise(Exception("Starting point and the reference point must be different!"))
+        
+        w_x = makeUnit(p2 - p1)
+        w_y = makeUnit(get_perp_component(p1 - p0, w_x))
+        w_z = makeUnit(np.cross(w_x, w_y))
+
+        new_helix = [p1 + p[0]*w_x + p[1]*w_y + p[2]*w_z for p in struct]
+        return new_helix
+
+    def interpolate_structure_with_nucleosomes(self):
+        """
+        Interpolate the 3D structure V with nucleosomes.
+        """    
+        # Normalize bw_array
+        bw_signal = np.log(self.bw + 1e-6)
+        if not np.all(bw_signal == bw_signal[0]):
+            bw_signal = self.min_max_scale(bw_signal)
+        elif self.bw[0] == 0:
+            bw_signal = np.zeros(bw_signal.shape)
+        else:
+            bw_signal = np.ones(bw_signal.shape)
+        
+        # Initialize interpolated structure
+        interpolated_structure = []
+        
+        # Interpolate each segment with nucleosomes
+        print('Building nucleosome structure...')
+        prev_zigzag = None
+        for i in tqdm(range(len(self.V) - 1)):
+            # Get segment endpoints
+            start_point = self.V[i]
+            end_point = self.V[i + 1]
+            
+            # Calculate number of nucleosomes in segment
+            num_nucleosomes = int(np.round(bw_signal[i] * self.max_nucs_per_bead))
+            
+            # Generate helices that represent nucleosomes
+            interpolated_structure.append([start_point])
+            if num_nucleosomes > 0:
+                helices, prev_zigzag = self.single_bead_nucgenerator(start_point, end_point, num_nucleosomes, 
+                                                                     prev_zigzag_vec=prev_zigzag)
+                interpolated_structure.extend(helices)
+            else:
+                prev_zigzag = None
+        interpolated_structure.append([self.V[-1]])
+
+        # Flatten the nested list to get a list of coordinate tuples
+        flattened_coords = [coord for sublist in interpolated_structure for coord in sublist]
+        print('Done! You have the whole structure with nucleosomes. ;)')
+        return np.array(flattened_coords)
+
+    def single_bead_nucgenerator(self, start_point, end_point, num_nucleosomes, prev_zigzag_vec=None, turns=1.65):
+        """
+        Generate helices for nucleosomes in a segment.
+        """
+        # Calculate segment vector
+        segment_vector = end_point - start_point
+        segment_vector_norm = makeUnit(segment_vector)
+
+        # Calculate nucleosome and linker parameters
+        linker_len = self.nuc_r * 3.45
+        nuc_height = self.nuc_r * 1
+
+        # Initialize helices
+        theta = np.linspace(0, turns* 2 * np.pi, self.nuc_points)
+        nucleosome = self.make_helix(self.nuc_r, theta, nuc_height)
+        helices = list()
+        
+        # Generate helices for each nucleosome
+        if prev_zigzag_vec is None:
+            zigzag_vec1 = makeUnit(get_perpendicular(segment_vector))
+        else:
+            zigzag_vec1 = makeUnit(get_perp_component(prev_zigzag_vec, segment_vector))
+            if all(v==0 for v in zigzag_vec1):
+                zigzag_vec1 = makeUnit(get_perpendicular(segment_vector))
+        zigzag_vec2 = makeUnit(np.cross(zigzag_vec1, segment_vector))
+        phi = 0
+        for i in range(num_nucleosomes):
+            helix_point = start_point + (i+1)/(num_nucleosomes+1)*segment_vector
+            zigzag_vec = linker_len/2*(np.cos(phi)*zigzag_vec1 + np.sin(phi)*zigzag_vec2)
+            p1 = helix_point + zigzag_vec - nuc_height/2*segment_vector_norm
+            p2 = helix_point + zigzag_vec + nuc_height/2*segment_vector_norm
+            helix = self.move_structure_to(nucleosome, helix_point, p1, p2)
+            helices.append(helix)
+            phi += np.pi if i%2 == 0 else np.pi + self.phi_norm
+
+        # zigzag vec for another segment
+        zigzag_vec = np.cos(phi)*zigzag_vec1 + np.sin(phi)*zigzag_vec2
+
+        return helices, zigzag_vec
+    
+def main():
+    # Example data
+    V = get_coordinates_cif('/home/skorsak/Data/simulation_results/GM12878_GW/MultiEM_minimized.cif')
+    print('Initial granularity of structure =', len(V))
+    bw = import_bw('/home/skorsak/Data/encode/ATAC-Seq/ENCSR637XSC_GM12878/ENCFF667MDI_pval.bigWig', len(V))  # Mock self.signal array
+
+    # Interpolate structure with nucleosomes
+    nuc_interpol = NucleosomeInterpolation(V, bw)
+    iV = nuc_interpol.interpolate_structure_with_nucleosomes()
+    print('Final Length of Nucleosome Interpolated Structure:', len(iV))
+    viz_structure(iV, r=0.1)
---
+++ plots.py
@@ -0,0 +1,138 @@
+import seaborn as sns
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import figure
+import pandas as pd
+from utils import *
+from sklearn.decomposition import PCA
+import pyvista as pv
+
+pv.set_jupyter_backend("server")
+color_dict = {-2:'#bf0020', -1:'#e36a24', 1:'#20c8e6',2:'#181385',0:'#ffffff'}
+comp_dict = {-2:'B2', -1:'B1', 1:'A2',2:'A1',0:'no compartment'}
+
+def plot_projection(struct_3D,Cs,save_path):
+    # Dimensionality Reduction
+    pca = PCA(n_components=2)
+    struct_2d = pca.fit_transform(struct_3D)
+    colors, comps = [], []
+    for c in Cs[:len(struct_3D)]: 
+        colors.append(color_dict[c])
+        comps.append(comp_dict[c])
+    
+    # Calculate Distances
+    dists = list()
+    for vec in struct_3D: dists.append(np.linalg.norm(vec))
+    dists = np.array(dists)
+
+    # Make dataframe
+    df = pd.DataFrame()
+    df['x'], df['y'], df['z'] = struct_3D[:,0], struct_3D[:,1], struct_3D[:,2]
+    df['x_PCA'],df['y_PCA'] = struct_2d[:,0], struct_2d[:,1]
+    df['distance'] = dists
+    df['subcomp'] = Cs
+    df['subcomp_text'] = comps
+    df.drop(df[df['subcomp']==0.0].index,inplace=True)
+
+    # Plot Distribution
+    figure(figsize=(8, 8), dpi=100)
+    sns.scatterplot(data=df,x='x_PCA',y='y_PCA',hue='subcomp', palette="coolwarm", alpha=0.5)
+    plt.xlabel('First Pricipal Component')
+    plt.ylabel('Second Pricipal Component')
+    plt.title('Scatter Plot of PCA 2D Projection')
+    plt.savefig(save_path+'plots/PCA.svg',format='svg',dpi=100)
+    plt.close()
+
+    # Plot more stuff
+    figure(figsize=(8, 8), dpi=100)
+    sns.kdeplot(data=df, x='x', y='y', palette="coolwarm", hue='subcomp', bw_adjust=.5)
+    plt.title('Subcompartment 2D Density Plot')
+    plt.savefig(save_path+'plots/density_subcomp.svg',format='svg',dpi=100)
+    plt.close()
+    
+    figure(figsize=(8, 5), dpi=100)
+    sns.kdeplot(data=df, x='distance', hue='subcomp', fill=True, palette='coolwarm')
+    plt.title('Subcompartment Density Plot')
+    plt.savefig(save_path+'plots/kde_subcomp.svg',format='svg',dpi=100)
+    plt.close()
+
+    figure(figsize=(8, 5), dpi=100)
+    sns.kdeplot(data=df,x='distance',fill=True)
+    plt.title('Density Plot')
+    plt.savefig(save_path+'plots/kde.svg',format='svg',dpi=100)
+    plt.close()
+    
+    figure(figsize=(10, 8), dpi=100)
+    sns.kdeplot(data=df, x='x_PCA', y='y_PCA', cmap="gnuplot2", shade=True,cbar=True)
+    plt.title('2D Density Plot')
+    plt.savefig(save_path+'plots/density.svg',format='svg',dpi=100)
+    plt.close()
+
+def polyline_from_points(points):
+    poly = pv.PolyData()
+    poly.points = points
+    the_cell = np.arange(0, len(points), dtype=np.int_)
+    the_cell = np.insert(the_cell, 0, len(points))
+    poly.lines = the_cell
+    return poly
+
+def viz_structure(V, colors=None, r=0.3, cmap='coolwarm'):
+    polyline = polyline_from_points(V)
+    polyline["scalars"] = np.arange(polyline.n_points)
+
+    if colors is not None:
+        colors = colors[:len(V)]
+        color_values = (colors - np.min(colors)) / (np.max(colors) - np.min(colors))  # Normalize colors
+        polyline["colors"] = color_values  # Set colors as point scalars
+        polymer = polyline.tube(radius=r)
+        polymer.plot(smooth_shading=True, cmap=cmap, scalars="colors", show_scalar_bar=False)
+    else:
+        polymer = polyline.tube(radius=r)
+        polymer.plot(smooth_shading=True, show_scalar_bar=False)
+
+def viz_chroms(sim_path):
+    cif_path = sim_path + '/MultiEM_minimized.cif'
+    chrom_idxs_path = sim_path + '/chrom_idxs.npy'
+    chrom_ends_path = sim_path + '/chrom_lengths.npy'
+    chrom_idxs = np.load(chrom_idxs_path)
+    chrom_ends = np.load(chrom_ends_path)
+    V = get_coordinates_cif(cif_path)
+    N = len(V)
+    chroms = np.zeros(N)
+    for i in range(len(chrom_ends)-1):
+        start, end = chrom_ends[i], chrom_ends[i+1]
+        chroms[start:end] = chrom_idxs[i]
+    viz_structure(V,chroms[:len(V)],cmap='rainbow')
+
+def get_heatmap(cif_file,viz=False,th=1,save=False,save_path=None,vmax=1,vmin=0):
+    '''
+    It returns the corrdinate matrix V (N,3) of a .pdb file.
+    The main problem of this function is that coordiantes are not always in 
+    the same column position of a .pdb file. Do changes appropriatelly,
+    in case that the data aren't stored correctly.
+    
+    Input:
+    file (Openmm Qunatity): an OpenMM vector of the form 
+    Quantity(value=[Vec3(x=0.16963918507099152, y=0.9815883636474609, z=-1.4776774644851685),
+    Vec3(x=0.1548253297805786, y=0.9109517931938171, z=-1.4084612131118774),
+    Vec3(x=0.14006929099559784, y=0.8403329849243164, z=-1.3392155170440674),
+    Vec3(x=0.12535107135772705, y=0.7697405219078064, z=-1.269935131072998),
+    ...,
+    unit=nanometer)
+    
+    Output:
+    H (np.array): a heatmap of the 3D structure.
+    '''
+    V = get_coordinates_cif(cif_file)
+    print('Matrix shape:',V.shape)
+    mat = distance.cdist(V, V, 'euclidean') # this is the way \--/
+    mat = 1/(mat+1)
+
+    if viz:
+        figure(figsize=(15, 12),dpi=500)
+        plt.imshow(mat,cmap="Reds",vmax=vmax,vmin=vmin)
+        if save: plt.savefig(save_path,format='svg',dpi=500)
+        plt.colorbar()
+        plt.show()
+        if save: np.save(save_path.replace("svg", "npy"),mat)
+    return mat
