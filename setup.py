c9cbc82756739d373878cf18b76845531898f47e
Sebastian Korsak <49608786+BlackPianoCat@users.noreply.github.com>
2024-09-24 17:27:27 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
     name='MultiMM',  # Package name
     version='1.0.0',  # Version of the software
     description='A tool for chromatin modeling from nucleosomes to chromosomal territories.',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
     author='Sebastian Korsak',
     author_email='s.korsak@datascience.edu.pl',
     url='https://github.com/SFGLab/MultiMM',  # GitHub repository URL
