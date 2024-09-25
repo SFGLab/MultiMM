68501211f0b7beb5224c04d591232679f891ab29
Sebastian Korsak <49608786+BlackPianoCat@users.noreply.github.com>
2024-09-25 11:23:06 +0200

    Update plots.py

--- simulation/plots.py
+++ simulation/plots.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.pyplot import figure
 import pandas as pd
-from utils import *
+from .utils import *
 from sklearn.decomposition import PCA
 import pyvista as pv
 
